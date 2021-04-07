4f16817dbd2cc3810c790faf3ce35d584b8e4058
hshthfhs <63581792+hshthfhs@users.noreply.github.com>
2021-04-07 21:43:07 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,214 +1,96 @@
-/**
- * const変数宣言
- * 再宣言上書き不可能
- */
-const val1 = "const変数";
-console.log(val1);
+import "./styles.css";
 
-/**
- * let変数
- * 再宣言不可能
- */
-let val2 = "let変数";
-console.log(val2);
-
-val2 = "let再宣言";
-console.log(val2);
-
-/**
- * constで定義したオブジェクトはプロパティの変更が可能
- * オブジェクトの定義はほぼconstを利用する
- */
-const val3 = {
-  name: "名前",
-  age: 25
+/** alertでJSが動くか確認 */
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化
+  const addText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  createIncompleteList(addText);
 };
 
-console.log(val3.name);
-console.log(val3.age);
+//　未完了リストに追加する関数
+const createIncompleteList = (text) => {
+  // div生成
+  const div = document.createElement("div");
 
-/**
- * constで定義した配列はプロパティの変更が可能
- */
-const val5 = ["dog", "cat"];
+  // class名をつける
+  div.className = "list-row";
 
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+  // li生成
+  const li = document.createElement("li");
+  // 入力された文字をliのテキストにする
+  li.innerText = text;
 
-//テンプレート文字列
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
 
-const name = "花田";
-const age = 25;
+  // クリックされたときのイベント
+  completeButton.addEventListener("click", () => {});
+  // button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
 
-const message = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message);
+  // 完了ボタンがクリックされたときのイベント
+  completeButton.addEventListener("click", () => {
+    //　押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromImcompleteLst(deleteButton.parentNode);
+    const addTarget = completeButton.parentNode;
+    // TODO内容テキスト取得
+    const text = addTarget.firstElementChild.innerText;
 
-/**
- * アロー関数
- */
-const fun = (str) => {
-  return str;
+    //　div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //addEventLisnnerは関数の呼び出し
+    backButton.addEventListener("click", () => {
+      //　押された戻すボタンの親タグ（div）を完了リストから削除
+      //　戻す要素を取得
+      const deleteTarget = backButton.parentNode;
+
+      //　完了リストから削除
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // 戻すテキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+  // 削除ボタンがクリックされたときのイベント
+  deleteButton.addEventListener("click", () => {
+    deleteFromImcompleteLst(deleteButton.parentNode);
+  });
+
+  // 未完了リストから指定の要素を削除
+  const deleteFromImcompleteLst = (target) => {
+    //　ボタンが押されたとき親要素のdivタグを削除
+    document.getElementById("imcomplete-list").removeChild(target);
+  };
+  // divタグの子要素にliタグを追加
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("imcomplete-list").appendChild(div);
 };
-console.log(fun("funです"));
 
-const fun1 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(fun1(20, 20));
-
-//分割代入
-
-const myProfile = {
-  name2: "花田",
-  age2: 30
-};
-const message1 = `名前は${myProfile.name1}です。　年齢は${myProfile.age1}歳です`;
-console.log(message1);
-
-/**
- * コンパクト
- */
-const { name1, age1 } = myProfile;
-const message2 = `名前は${name1}です。年齢は${age1}歳です`;
-console.log(message2);
-
-/**
- * いろんな方法
- */
-const myProfile2 = ["武", 35];
-
-const message3 = `名前は${myProfile2[0]}です。　年齢は${myProfile2[1]}歳です`;
-console.log(message3);
-
-const [name2, age2] = myProfile2;
-const message4 = `名前は${name2}です。年齢は${age2}歳です`;
-console.log(message4);
-
-/**
- * デフォルト値、引数など
- */
-// (name = "ゲスト") 初期値設定
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
-sayHello("花田");
-
-/**
- * スプレッド構文
- * 順番に処理する
- */
-// 配列の展開
-const arr1 = [1, 2];
-console.log(arr1);
-console.log(...arr1);
-
-const sumFunc = (num1, num2) => console.log(num1 + num2);
-sumFunc(...arr1);
-
-//まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
-
-//配列のコピー
-const arr4 = [10, 40];
-const arr5 = [30, 50];
-
-const arr6 = [...arr4];
-console.log(arr6);
-
-const arr7 = [...arr4, ...arr5];
-console.log(arr7);
-
-/**
- * map filter を使った配列の処理
- */
-// const nameArr = ["田中", "山田", "花田"];
-
-// const nameArr2 = nameArr.map((name) => {
-//   return name;
-// });
-// console.log(nameArr2);
-
-/**
- * for文と一緒
- */
-const nameArr = ["田中", "山田", "花田"];
-nameArr.map((name) => console.log(name));
-
-/**
- * filterは条件式を書くことができる
- */
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
-
-nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
-
-/**
- *
- */
-const newNameArr = nameArr.map((name) => {
-  if (name === "花田") {
-    return name;
-  } else {
-    return `'${name}さん`;
-  }
-});
-console.log(newNameArr);
-
-/**
- *　三項演算子
- */
-//ある条件？条件がtrueのとき : 条件がfalseのとき
-const val6 = 1 > 0 ? "true" : "false";
-console.log(val6);
-
-//toLocaleStringは3桁区切りにしてくれる
-const num = 1200;
-console.log(num.toLocaleString());
-
-//numの文字列判定　"number"数値判定
-const formattedNum =
-  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
-console.log(formattedNum);
-
-/**
- * 数値判定
- */
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲";
-};
-console.log(checkSum(50, 100));
-
-/**
- * 論理演算しの本当の意味　&& ||
- */
-const flag1 = true;
-const flag2 = false;
-
-if (flag1 || flag2) {
-  console.log(true);
-}
-
-if (flag1 && flag2) {
-  console.log(false);
-}
-
-/**
- * 変数が設定されていなければ、
- * 左側（numNull）がfalseであれば、右側("金額未設定です")を返す
- */
-const numNull = null;
-const fee = numNull || "金額未設定です";
-console.log(fee);
-
-/**
- * null判定
- */
-const numNull2 = 100;
-const fee2 = numNull2 && "何か設定されました";
-console.log(fee2);
+/** idを取得してクリックイベントが起きた時にonClickAdd関数を呼び出す*/
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
